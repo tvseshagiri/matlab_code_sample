cb6a3313b006e12152e6ee9f8a6e47b6a3ed9fc6
Seshagiri Telkapalli <tvseshagiri@gmail.com>
2024-10-24 21:06:15 +0530

    Add files via upload

--- query_builder.py
+++ query_builder.py
@@ -5,6 +5,7 @@ from langchain_core.prompts import PromptTemplate
 from datetime import datetime
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import PydanticOutputParser
 
 
 class SearchFilter(BaseModel):
@@ -18,7 +19,7 @@ class SearchFilter(BaseModel):
 
 
 llm = ChatGroq(model="llama3-groq-70b-8192-tool-use-preview", temperature=0)
-# .with_structured_output(SearchFilter)
+
 
 prompt = PromptTemplate.from_template(
     """
@@ -28,28 +29,40 @@ prompt = PromptTemplate.from_template(
         Query: {input}
     """
 )
-prompt = prompt.partial(today=datetime.today().strftime("%Y-%m-%d"))
-print(datetime.today().strftime("%Y-%m-%d"))
-chain = prompt | llm
-
-
-system = """You are an expert at converting user questions into database queries. 
-You have access to a database of customer meeting interaction notes for building LLM-powered applications. 
-Given a question, return a list of database queries optimized to retrieve the most relevant results.
-For relative data calculations like next 3 quarters, past 3 months etc, always calculate from Today: {today}.
-
-If there are acronyms or words you are not familiar with, do not try to rephrase them."""
+parser = PydanticOutputParser(pydantic_object=SearchFilter)
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", system),
-        ("human", "{question}"),
+        (
+            "system",
+            """For relative data calculations like next 3 quarters, past 3 months etc, always calculate from Today: {today}.
+        rewrite the query for better semantic search results.
+        Answer the user query. Wrap the output in `json` tags\n{format_instructions}""",
+        ),
+        ("human", "{query}"),
     ]
-)
+).partial(format_instructions=parser.get_format_instructions())
+
+
 prompt = prompt.partial(today=datetime.today().strftime("%Y-%m-%d"))
+# system = """You are an expert at converting user questions into database queries.
+# You have access to a database of customer meeting interaction notes for building LLM-powered applications.
+# Given a question, return a list of database queries optimized to retrieve the most relevant results.
+# For relative data calculations like next 3 quarters, past 3 months etc, always calculate from Today: {today}.
+
+# If there are acronyms or words you are not familiar with, do not try to rephrase them."""
+# prompt = ChatPromptTemplate.from_messages(
+#     [
+#         ("system", system),
+#         ("human", "{question}"),
+#     ]
+# )
+# prompt = prompt.partial(today=datetime.today().strftime("%Y-%m-%d"))
 structured_llm = llm.with_structured_output(SearchFilter)
-query_analyzer = {"question": RunnablePassthrough()} | prompt | structured_llm
+query_analyzer = {"query": RunnablePassthrough()} | prompt | structured_llm
 
 
-resp = query_analyzer.invoke("what are the actionable items for the LG")
+resp = query_analyzer.invoke(
+    "what are the actionable items for the LG for past 3 months"
+)
 
 print(resp)
